d134b5a58bdd924d02a25171e650c1e9d442a24c
vinayak <itzvinvin@gmail.com>
2017-10-20 21:33:45 +0530

    intial checkin of sales tool

---
+++ SalesTol/src/ca/demo/salestool/Main.java
@@ -0,0 +1,25 @@
+package ca.demo.salestool;
+
+public class Main {
+
+	
+	
+	
+public static void displaygreeting()
+{
+	System.out.println("hello happy sales");
+	System.out.println("this app shoows sales data");
+}
+
+
+public static void main(String[] args) {
+	
+	SalesData data =new SalesData();
+	displaygreeting();
+	data.display();
+	
+	
+}
+
+
+}
