287a7220e3804f772adf91967585af0aff8acdd5
itzvinayak <itzvinvin@gmail.com>
2017-10-21 13:02:46 +0530

    added a second test output

--- SalesTol/src/ca/demo/salestool/Main.java
+++ SalesTol/src/ca/demo/salestool/Main.java
@@ -10,6 +10,7 @@ public static void displaygreeting()
 	System.out.println("hello happy sales");
 	System.out.println("this app shoows sales data");
 	System.out.println("test 1");
+	System.out.println("test 2");
 }
 
 
